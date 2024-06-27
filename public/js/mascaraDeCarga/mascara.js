be928d69833231a2967c83ea734d6886d3f4daa3
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2024-06-27 17:15:35 -0600

    CV-Portafolio

---
+++ public/js/mascaraDeCarga/mascara.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    mascaraObloqueo();
+});
+
+function mascaraObloqueo() {
+    $.blockUI({
+        //AQUI SOLO CAMBIA LA IMG Y COLOCA EL ORDEN DE TUS CARPETAS PARA QUE FUNCIONE BIEN
+        //Nota: Si Tienes Tu Carpeta De Img En Una Carpeta Public Puede Causar Fallas De Que No Agarre La Img Solo Saca La Carpeta Img De La Carpeta Publica Y Listo
+        message: ' <img src="../../../img/LogoPersonal.webp" class="img-fluid" width="300" height="300">',
+        css: {
+            border: 'none',
+            // padding: '15px', 
+            backgroundColor: 'rgba(138,221,45,0)',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            // opacity: .5, 
+            color: '#FFFFFF '
+        }
+    });
+
+    setTimeout($.unblockUI, 800);
+    
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    let altura = document.getElementById('menu').offsetTop;
+    window.addEventListener("scroll", () => {
+        if (window.scrollY  > altura) {
+           document.getElementById('menu').classList.add('menu-fixed');
+        } else {
+           document.getElementById('menu').classList.remove('menu-fixed');
+        }
+    });
+
+});
